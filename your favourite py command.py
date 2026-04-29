80856aa29525f730fa3c55df74b31395c3c2a1ee
Giosuè <giosuegiovagnoni@gmail.com>
2026-04-29 19:28:44 +0200

    Delete your favourite py command.py

--- your favourite py command.py
+++
@@ -1,9 +0,0 @@
-# Whit print you can print text on your screen
-print ("Welcome to: your favourite pyton command!")
-# import random takes a random thing from the list tat we will create
-import random
-# Here is the list! just gave to him a name and than use the symbol = and write everithing in [""]
-commands=["Print","Import random","variables","EVERYONE!"]
-# Let's print a random one whit random.choice and gave the name of the list!
-print (random.choice (commands))
-# CONGRATS! You written your firs code that takes a random world. Continue exploring! 
